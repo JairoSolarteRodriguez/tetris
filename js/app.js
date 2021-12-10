c123209cec327cb184a8597afabb23dde6a8f895
Santiago López <tsgustavo1@gmail.com>
2021-12-10 12:19:28 -0500

    Agregar Tablero Tetris

---
+++ js/app.js
@@ -0,0 +1,64 @@
+let Tetris = function () {
+
+    //Variable del tablero
+    this.CANVAS;
+    //variable contexto
+    this.CTX;
+
+    //FPS
+
+    this.FPS = 50;
+
+    //Definir el ancho y alto del escenatio
+
+    this.anchoTablero = 41;
+    this.altoTablero = 41;
+
+    //Array
+
+
+
+    this.tablero = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ];
+
+
+    //construir tablero
+    constructor(canvasId){
+        for (ejeY = 0; ejeY < this.tablero.length; ejeY++){
+            //recorrer subíndices
+            for (ejeX = 0; ejeX < this.tablero[ejeY].length; ejeX++){
+                if(this.tablero[ejeY][ejeX] == 0){
+                    CTX.fillStyle = 'grenn';
+                    CTX.fillRect(ejeX*this.anchoTablero, ejeY*this.altoTablero, this.anchoTablero, this.altoTablero )
+                }
+            } 
+        }        
+
+    }
+
+    
+
+}
+
+
