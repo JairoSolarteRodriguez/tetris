2c48b3db8d403b4cd518d0b507329d86e2724bd2
Santiago López <tsgustavo1@gmail.com>
2021-12-10 12:49:47 -0500

    constructor

--- js/app.js
+++ js/app.js
@@ -1,64 +1,61 @@
-let Tetris = function () {
+class Tetris {
+    constructor() {
 
-    //Variable del tablero
-    this.CANVAS;
-    //variable contexto
-    this.CTX;
+        //Variable del tablero
+        this.CANVAS;
+        //variable contexto
+        this.CTX;
 
-    //FPS
+        //FPS
+        this.FPS = 50;
 
-    this.FPS = 50;
+        //Definir el ancho y alto del escenatio
+        this.anchoTablero = 41;
+        this.altoTablero = 41;
 
-    //Definir el ancho y alto del escenatio
-
-    this.anchoTablero = 41;
-    this.altoTablero = 41;
-
-    //Array
-
-
-
-    this.tablero = [
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-    ];
+        //Array
+        this.tablero = [
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        ];
 
 
-    //construir tablero
-    constructor(canvasId){
-        for (ejeY = 0; ejeY < this.tablero.length; ejeY++){
-            //recorrer subíndices
-            for (ejeX = 0; ejeX < this.tablero[ejeY].length; ejeX++){
-                if(this.tablero[ejeY][ejeX] == 0){
-                    CTX.fillStyle = 'grenn';
-                    CTX.fillRect(ejeX*this.anchoTablero, ejeY*this.altoTablero, this.anchoTablero, this.altoTablero )
+        //construir tablero
+        constructor(canvasId); {
+            for (ejeY = 0; ejeY < this.tablero.length; ejeY++) {
+                //recorrer subíndices
+                for (ejeX = 0; ejeX < this.tablero[ejeY].length; ejeX++) {
+                    if (this.tablero[ejeY][ejeX] == 0) {
+                        CTX.fillStyle = 'grenn';
+                        CTX.fillRect(ejeX * this.anchoTablero, ejeY * this.altoTablero, this.anchoTablero, this.altoTablero);
+                    }
                 }
-            } 
-        }        
+            }
+
+        }
+
+
 
     }
-
-    
-
 }
 
 
