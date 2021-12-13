21236e05cbf94ef83cd9a14d9b4b1463f777165e
Santiago López <tsgustavo1@gmail.com>
2021-12-13 09:33:09 -0500

    Cambios para hacer Merge

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-//Array talero
-let tablero = [
+//Array board
+let board = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -23,31 +23,39 @@ let tablero = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
+//Draw tiles
 let tetromino = [
     [
-        [1, 1, 1],
-        [0, 1, 0],
+        [1, 1, 1, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
     ],
     [
-        [1, 1, 0],
-        [0, 1, 1],
+        [0, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 0, 0],
     ],
     [
-        [0,0,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
+        [0,1,0,0],
     ],
     [
-        [0,0],
-        [0,0],
+        [1,1],
+        [1,1],
     ],
     [
-        [0],
-        [0],
-        [0],
-        [0,0],
+        [0,1,0],
+        [0,1,0],
+        [0,1,1],
     ]
 ];
 
-let colores = [
+//token colors
+let colors = [
     "#ffd300",
     "#de38c8",
     "#652ec7",
@@ -67,46 +75,53 @@ let colores = [
     "#fd971f",
 ];
 
+//refresh variable
+let lastTime = 0;
+
 
 
 class Tetris{
-    //construir tablero
-    constructor(canvasId, tablero) {
-        // Obtenemos el tablero que pasamos por parametros
-        this.tablero = tablero;
+    //construir board
+    constructor(canvasId, board) {
+        // we choose the board that we pass by parameter
+        this.board = board;
 
-        //Variable del tablero
+        //board variable
         this.CANVAS = document.getElementById(canvasId);
 
-        //variable contexto
+        // context variable
         this.ctx = this.CANVAS.getContext('2d');
         this.FPS = 50;
 
-        //Definir el ancho y alto del la cuadricula
-        this.anchoCuadricula = 41;
-        this.altoCuadricula = 41;
+        //Define the width and height of the grid
+        this.gridWidth = 41;
+        this.gridHeigth = 41;
 
-        // Definimos el ancho y alto del tablero deacuerdo a las medidas de la cuadricula
-        this.CANVAS.width = this.anchoCuadricula * 15;
-        this.CANVAS.height = this.anchoCuadricula * 20;
+        // We define the width and height of the board according to the measurements of the grid
+        this.CANVAS.width = this.gridWidth * 15;
+        this.CANVAS.height = this.gridWidth * 20;
 
-        // Recorremos el primer nivel del array
-        for (let ejeY = 0; ejeY < this.tablero.length; ejeY++) {
-            //recorrer subíndices
-            for (let ejeX = 0; ejeX < this.tablero[ejeY].length; ejeX++) {
-                if (this.tablero[ejeY][ejeX] == 0) {
+        // We go through the first level of the array
+        for (let ejeY = 0; ejeY < this.board.length; ejeY++) {
+            //loop subscripts
+            for (let ejeX = 0; ejeX < this.board[ejeY].length; ejeX++) {
+                if (this.board[ejeY][ejeX] == 0) {
                     this.ctx.fillStyle = 'green';
-                    this.ctx.fillRect(ejeX * this.anchoCuadricula, ejeY * this.altoCuadricula, this.anchoCuadricula, this.altoCuadricula);
+                    this.ctx.fillRect(ejeX * this.gridWidth, ejeY * this.gridHeigth, this.gridWidth, this.gridHeigth);
                     this.ctx.strokeStyle = '#eee';
-                    this.ctx.strokeRect(ejeX * this.anchoCuadricula, ejeY * this.altoCuadricula, this.anchoCuadricula, this.altoCuadricula)
+                    this.ctx.strokeRect(ejeX * this.gridWidth, ejeY * this.gridHeigth, this.gridWidth, this.gridHeigth)
                 }
             }
         }        
     }
     //Selection of colors and shapes randomly
-    getRandom (array) {
+    getRandom (colors,) {
         return Math.floor(Math.random() * array.length)
     }
+
+    
+    //Random color is assigned to random Tetromino
+    
 
 }
 
@@ -115,6 +130,28 @@ class Tetris{
 
 
 
+
+
+
+
+
+ function update (time = 0) {
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    Tetris ()
+    requestAnimationFrame(update); 
+
+    
+}
+
+
+
+
 // Creacion del objeto
-let Tablero = new Tetris('canvas', tablero);
+let Tablero = new Tetris('canvas', board);
+update(50)
+
+//create update 
+
+
 
